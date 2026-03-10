f54eb5f0e5d9ffb5f75a6697d439d1c89a799c9e
Advansed <atlasov.n.r@gmail.com>
2026-03-10 10:24:15 +0900

    11

--- src/Store/api.ts
+++ src/Store/api.ts
@@ -1,28 +1,28 @@
 
-const url = '/mi/'
+const url = 'https://fhd.aostng.ru/mi/'
 
 export interface TResponse {
-  success:          boolean;
-  data:             any;
-  message:         string;
+    success: boolean;
+    data: any;
+    message: string;
 }
 
 export const EMPTY_USER: TResponse = {
-    success:        false,
-    data:           {},
-    message:       "Ошибка в клиенте"
+    success: false,
+    data: {},
+    message: "Ошибка в клиенте"
 }
 
-export async function post(method: string, params:any) {
+export async function post(method: string, params: any) {
     try {
-        const response = await fetch( url + method, {
+        const response = await fetch(url + method, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: JSON.stringify( params )
+            body: JSON.stringify(params)
         });
-                
+
         const data = await response.json();
         return data;
     } catch (error) {
@@ -31,16 +31,16 @@ export async function post(method: string, params:any) {
     }
 }
 
-export async function getData(method: string, params:any) {
+export async function getData(method: string, params: any) {
     try {
-        const response = await fetch( 'https://fhd.aostng.ru/inter_vesta/hs/API_STNG/V2/' + method, {
+        const response = await fetch('https://fhd.aostng.ru/inter_vesta/hs/API_STNG/V2/' + method, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
-            body: JSON.stringify( params )
+            body: JSON.stringify(params)
         });
-                
+
         const data = await response.json();
         return data;
     } catch (error) {
@@ -49,10 +49,10 @@ export async function getData(method: string, params:any) {
     }
 }
 
-export const formatSum      = (sum: number): string => {
-  return new Intl.NumberFormat('ru-RU', {
-    style: 'currency',
-    currency: 'RUB',
-    minimumFractionDigits: 2
-  }).format(sum);
+export const formatSum = (sum: number): string => {
+    return new Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: 'RUB',
+        minimumFractionDigits: 2
+    }).format(sum);
 };
